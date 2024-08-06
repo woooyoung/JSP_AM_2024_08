19998fd4641fd9bff525cc44c249bd72cc6511ca
woooyoung <axdsw121@gmail.com>
2024-08-06 14:31:22 +0900

    SecSql 도입, SQL 인젝션

--- src/main/java/com/KoreaIT/java/jsp_AM/servlet/ArticleDetailServlet.java
+++ src/main/java/com/KoreaIT/java/jsp_AM/servlet/ArticleDetailServlet.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 
 import com.KoreaIT.java.jsp_AM.util.DBUtil;
+import com.KoreaIT.java.jsp_AM.util.SecSql;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -43,8 +44,11 @@ public class ArticleDetailServlet extends HttpServlet {
 
 			int id = Integer.parseInt(request.getParameter("id"));
 
-//			String sql = "SELECT * FROM article ORDER BY id DESC";
-			String sql = String.format("SELECT * FROM article WHERE id = %d", id);
+//			String sql = String.format("SELECT * FROM article WHERE id = %d", id);
+
+			SecSql sql = SecSql.from("SELECT *");
+			sql.append("FROM article");
+			sql.append("WHERE id = ?", id);
 
 			Map<String, Object> articleRow = DBUtil.selectRow(conn, sql);
 
