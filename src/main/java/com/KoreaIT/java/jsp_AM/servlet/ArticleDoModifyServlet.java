27dc1919fd58b42a6baa26872534ecba8be86053
woooyoung <axdsw121@gmail.com>
2024-08-07 10:56:38 +0900

    게시글 수정 기능 구현

---
+++ src/main/java/com/KoreaIT/java/jsp_AM/servlet/ArticleDoModifyServlet.java
@@ -0,0 +1,77 @@
+package com.KoreaIT.java.jsp_AM.servlet;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.KoreaIT.java.jsp_AM.util.DBUtil;
+import com.KoreaIT.java.jsp_AM.util.SecSql;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/article/doModify")
+public class ArticleDoModifyServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+
+			throws ServletException, IOException {
+		response.setContentType("text/html;charset=UTF-8");
+
+		// DB 연결
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("클래스 x");
+			e.printStackTrace();
+		}
+
+		String url = "jdbc:mysql://127.0.0.1:3306/24_08_JAM?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul";
+
+		String user = "root";
+		String password = "";
+
+		Connection conn = null;
+
+		try {
+			conn = DriverManager.getConnection(url, user, password);
+
+			int id = Integer.parseInt(request.getParameter("id"));
+
+			String title = request.getParameter("title");
+			String body = request.getParameter("body");
+
+			SecSql sql = SecSql.from("UPDATE article");
+			sql.append("SET ");
+			sql.append("title = ?,", title);
+			sql.append("`body` = ?", body);
+			sql.append("WHERE id = ?;", id);
+
+			DBUtil.update(conn, sql);
+
+			response.getWriter().append(
+					String.format("<script>alert('%d번 글이 수정 됨'); location.replace('detail?id=%d');</script>", id, id));
+
+		} catch (SQLException e) {
+			System.out.println("에러 1 : " + e);
+		} finally {
+			try {
+				if (conn != null && !conn.isClosed()) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
